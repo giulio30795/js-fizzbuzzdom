f09b23e240f27512affeb62b823c66063741f4c8
giulio30795 <giulioincarbone95@gmail.com>
2021-10-25 17:18:40 +0200

    done

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
-// Scrivi un programma che esegua un ciclo da 1 a 100 e a ogni iterazione appenda un elemento html al container con uno stile differente a seconda del valore dell’indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5 (vedi grafica allegata).
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Come faccio a sapere se un numero è divisibile per? Abbiamo visto qualcosa di particolare che possiamo usare? Come creare nuovi elementi html e appenderli al container?
 
 const mainBox = document.querySelector('.main-box')
 
-    for (let i = 1; i <= 100; i++) {
-        mainBox.innerHTML += `<div class="box ${i}" >${i}</div>`;
-    }
-
-        if ((i % 3) && (i % 5)){
+    for (let i = 1; i <= 100; i++){
+        
+        if (i % 3 == 0 && i % 5 ==0) {
+            mainBox.innerHTML += `<div class="box red" >fizzbuzz</div>`;
         }
-    
+
+        else if (i % 3 == 0) {
+            mainBox.innerHTML += `<div class="box green" >fizz</div>`;
+        }
+
+        else if (i % 5 ==0) {
+            mainBox.innerHTML += `<div class="box yellow" >buzz</div>`;
+        }
+
+        else{
+            mainBox.innerHTML += `<div class="box" >${i}</div>`;
+        }
+    }
